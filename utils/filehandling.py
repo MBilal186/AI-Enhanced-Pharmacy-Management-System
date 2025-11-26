49aaca102f713e7f755967076eb29276b7848b31
mirteza1010-blip <mirteza1010@gmail.com>
2025-11-26 22:35:09 +0500

    debugging

--- utils/filehandling.py
+++ utils/filehandling.py
@@ -1,13 +1,13 @@
 import csv
 def read_csv(filename):
     data = []
-    with open(filename, "r", newline="", encoding="utf-8") as file:
+    with open(filename , newline="" , mode = "r") as file:
         reader = csv.reader(file)
         for row in reader:
             data.append(row)
     return data
 
 def write_csv(filename, data):
-    with open(filename, "w", newline="", encoding="utf-8") as file:
+    with open(filename , newline="", mode="w") as file:
         writer = csv.writer(file)
         writer.writerows(data)
